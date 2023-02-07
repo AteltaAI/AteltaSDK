369a0aced745eeee961c9c29b7802d00ebea4184
Anindyadeep <Anindyadeep@users.noreply.github.com>
2023-02-07 16:19:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="AteltaAI"
+original_name="ateltasdk"
+original_urlname="AteltaSDK"
+original_description="Awesome ateltasdk created by AteltaAI"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ateltasdk $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
