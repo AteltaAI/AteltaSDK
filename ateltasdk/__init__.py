08d119b4464637a0d55e54882efcca17ee25c9ac
Anindyadeep <2005641@kiit.ac.in>
2023-02-09 23:22:17 +0530

    drawing functions and two-streamer

--- ateltasdk/__init__.py
+++ ateltasdk/__init__.py
@@ -1,2 +1,12 @@
-from pose.pose_matcher import * 
-from pose.pose_utils import * 
+from ateltasdk.pose.pose_matcher import (
+    pose_matcher
+)
+
+from ateltasdk.pose.pose_utils import (
+    get_angle, 
+    convert_pose_landmark_to_list, 
+    convert_mediapipe_pose_results_to_json, 
+)
+
+from ateltasdk import streamer
+from ateltasdk.pose.draw import PoseDraw
